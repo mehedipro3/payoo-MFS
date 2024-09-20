3ac07caa525e2ad16bcba0770c9003f0ba6daf8f
mehedipro3 <mehediratul37@gmail.com>
2024-09-21 00:32:51 +0600

    home section are add

--- js/login.js
+++ js/login.js
@@ -1,6 +1,14 @@
-document.getElementById('btn-login').addEventListener('click',function(event){
-event.preventDefault();
+document.getElementById('btn-login').addEventListener('click', function (event) {
+  event.preventDefault();
   const phoneNumber = document.getElementById('phone-number').value;
-  console.log(phoneNumber);
-  
+  const pinNumber = document.getElementById('pin-number').value;
+  ///////// this system is temporary ///////
+  if (phoneNumber == '01312415417' && pinNumber === '1234') {
+    console.log('Yor are login');
+    window.location.href = '/home.html';
+  }
+  else {
+    alert('Wrong number or pin');
+
+  }
 })
