4f9ed5d25ca0e5638d8b585368d2c740dd6ae213
mehedipro3 <mehediratul37@gmail.com>
2024-09-21 00:57:31 +0600

    input form added

---
+++ js/home.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-add-money')
+  .addEventListener('click',function(event){
+    event.preventDefault();
+    const inputAddMoney = document.getElementById('input-add-money').value;
+    const inputPinNumber = document.getElementById('input-pin-number').value;
+    
+  })
