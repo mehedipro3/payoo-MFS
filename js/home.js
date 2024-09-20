b58f04ae55c60dd6aaacb353735e9cf3bbc84870
mehedipro3 <mehediratul37@gmail.com>
2024-09-21 01:18:26 +0600

    balance section added

--- js/home.js
+++ js/home.js
@@ -3,5 +3,15 @@ document.getElementById('btn-add-money')
     event.preventDefault();
     const inputAddMoney = document.getElementById('input-add-money').value;
     const inputPinNumber = document.getElementById('input-pin-number').value;
-    
+    if(inputPinNumber == '1234'){
+      const accountBalance = document.getElementById('account-balance').innerText;
+      const Balance = parseFloat(accountBalance);
+      const account = parseFloat(inputAddMoney);
+      const newBalance = account + Balance ;
+      document.getElementById('account-balance').innerText = newBalance;
+      
+    }
+    else{
+      alert('please try again');
+    }
   })
